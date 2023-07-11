f8f9aec7d25bcd2512fc112c64cef11f125b53e5
Naveen Srikanth K <132446865+knsk96@users.noreply.github.com>
2023-07-11 12:39:23 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "tf_elb_0007"
   availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "tf_elb_007"
   }
 }
 
