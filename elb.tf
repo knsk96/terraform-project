16b959d2302408de99ee1d47653a258112d755e3
Naveen Srikanth K <132446865+knsk96@users.noreply.github.com>
2023-07-12 12:15:03 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "tfelbbar01"
+  name               = "elbtf"
   availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
@@ -25,14 +25,3 @@ resource "aws_elb" "bar" {
   }
 }
 
- /*resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "aurora-cluster-demo"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["us-east-1a", "us-east-1b"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "Rahamshaik#444555"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-}*/
