f0de86e649df10bb0078ad28a6e84a72ab5bc6f1
Naveen Srikanth K <132446865+knsk96@users.noreply.github.com>
2024-02-29 11:41:52 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "elbbynani8143972024"
+  name               = "nanielb2024"
   availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "elbbynani814320"
+    Name = "nanielb2024"
   }
 }
 
